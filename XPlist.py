364f669b19a564ef66d7f03384bc9646db01b7b6
Arnaud <noar@melomac.com>
2014-10-07 18:20:04 +0200

    updated SL, Lion and ML URLs for https

--- XPlist.py
+++ XPlist.py
@@ -8,11 +8,11 @@ from Foundation import NSArray, NSDictionary, NSString, NSURL
 # ---------------------------------------------------------------------------
 
 XPROTECTS = [ { "name": "Snow Leopard",
-				"url" : "http://configuration.apple.com/configurations/macosx/xprotect/1/clientConfiguration.plist" },
+				"url" : "https://configuration.apple.com/configurations/macosx/xprotect/1/clientConfiguration.plist" },
 			  { "name": "Lion",
-				"url" : "http://configuration.apple.com/configurations/macosx/xprotect/2/clientConfiguration.plist" },
+				"url" : "https://configuration.apple.com/configurations/macosx/xprotect/2/clientConfiguration.plist" },
 			  { "name": "Mountain Lion",
-				"url" : "http://configuration.apple.com/configurations/macosx/xprotect/3/clientConfiguration.plist" },
+				"url" : "https://configuration.apple.com/configurations/macosx/xprotect/3/clientConfiguration.plist" },
 			  { "name": "Mavericks",
 				"surl": "https://swscan.apple.com/content/catalogs/others/index-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz" },
 			  { "name": "Yosemite",
