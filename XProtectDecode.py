10ef4d28baa226ed515a512bef1ebd7c7ac54c69
Arnaud <noar@melomac.com>
2014-10-08 22:03:29 +0200

    added BBEdit Text Filter

---
+++ XProtectDecode.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+# BBEdit Text Filter
+# 
+# Parse and Decode Identities and Patterns
+# for a quicker, but no so accurate, overview.
+
+import re
+from sys import stdin, stdout
+
+# ---------------------------------------------------------------------------
+
+def filter(text):
+	lines = text.splitlines()
+
+	i = 0
+	for line in lines:
+		
+		if line.find("<key>Identity</key>") != -1:
+			# SHA1 hex digest			
+			
+			if lines[i+3].find("</data>") != -1:
+				# <data>
+				# wIAM1QlbKNpLbKAUaKJ5+1vmkho=
+				# </data>
+				lines[i+2] = process_identity(lines[i+2])
+			
+			else:
+				# <data>
+				# A/ziWngj5jE5
+				# dSUGZo7t7a5N
+				# M7c=
+				# </data>
+				lines[i+2] = process_identity(lines[i+2] + lines[i+3].strip() + lines[i+4].strip())
+				lines.pop(i+4)
+				lines.pop(i+3)
+		
+		elif line.find("<key>Pattern</key>") != -1:
+			# Regular expression
+			lines[i+1] = process_pattern(lines[i+1])
+
+		i += 1
+
+	return "\n".join(lines)
+
+def process_identity(line):
+	r = re.compile("([\t]+)(.+)")
+	p = r.findall(line)
+
+	return "{pad}{sha}".format(
+		pad = p[0][0],
+		sha = p[0][1].decode("base64").encode("hex")
+		)
+
+def process_pattern(line):
+	r = re.compile("([\t]+)<string>(.+)</string>")
+	p = r.findall(line)
+
+	return "{pad}<string>\n{pattern}\n{pad}</string>".format(
+		pad = p[0][0],
+		pattern = _process_pattern(p[0][1], p[0][0])
+		)
+
+def _process_pattern(pattern, pad):
+	result = []
+
+	for p in pattern.split("*"):
+		try:
+			result.append("{pad}STR: {str}".format(
+				pad = pad,
+				str = p.decode("hex"))
+			)
+		
+		except:
+			result.append("{pad}HEX: {hex}".format(
+				pad = pad,
+				hex = p)
+			)
+
+	return "\n".join(result)
+
+# ---------------------------------------------------------------------------
+
+if __name__ == "__main__":
+	text = stdin.read()
+	stdout.write(filter(text))
