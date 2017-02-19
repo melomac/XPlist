91e447df27f679ad8b0353b50d536226a3893b01
Arnaud <noar@melomac.com>
2017-02-19 23:53:49 +0100

    tar also extract XProtect.yara

--- XPlist.py
+++ XPlist.py
@@ -41,7 +41,7 @@ def init_with_sucatalog(sucatalog, pkg_re = "^.*/XProtectPlistConfigData.*\.pkg$
 	
 	cmds = [ "/usr/bin/curl -s -o '%s' '%s'" %(tmp_pkg, pkg_url),
 			 "/usr/sbin/pkgutil --expand '%s' '%s'" %(tmp_pkg, tmp_dir),
-			 "/usr/bin/tar xf '%s' -C '%s' -s '|.*/||' --include '*.plist'" %(tmp_tar, tmp_dir) ]
+			 "/usr/bin/tar xf '%s' -C '%s' -s '|.*/||' --include '*/XProtect.*'" %(tmp_tar, tmp_dir) ]
 	
 	for cmd in cmds:
 		retcode = os.system(cmd)
